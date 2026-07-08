bcc82f7288e63f9b86ffd746b4c2369532bd889e
yasin culha <culhayasin732@gmail.com>
2026-07-08 22:17:43 +0300

    debug6

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,17 @@ use App\Services\ThemeRenderer;
 use App\Models\CompanyThemeSetting;
 
 // Ana Sayfa (Tanıtım)
+Route::domain('{subdomain}.apollonmedya.net')->group(function () {
+
+    // Birisi direkt test.apollonmedya.net olarak ana sayfaya geldiğinde çalışacak rota
+    Route::get('/', function ($subdomain) {
+        dd('DOMAIN ROUTE', $subdomain);
+    });
+    Route::get('/host-test', function () {
+    dd('DOMAIN ROUTE',
+            request()->route()->parameters(), request()->getHost());
+});
+
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
@@ -57,16 +68,7 @@ Route::get('/company/theme-editor/{theme}',[AdminController::class, 'themeEditor
 Route::post('/company/theme/save',[AdminController::class, 'saveTheme'])->name('company.theme.save');
 
 //MÜŞTERİ SUBDOMAINLERİ İÇİN ROTA GRUBU
-Route::domain('{subdomain}.apollonmedya.net')->group(function () {
 
-    // Birisi direkt test.apollonmedya.net olarak ana sayfaya geldiğinde çalışacak rota
-    Route::get('/', function ($subdomain) {
-        dd('DOMAIN ROUTE', $subdomain);
-    });
-    Route::get('/host-test', function () {
-    dd('DOMAIN ROUTE',
-            request()->route()->parameters(), request()->getHost());
-});
     
     
     // İleride firmanın sitesi içindeki diğer sayfalar için (Örn: test.apollonmedya.net/hakkimizda)
