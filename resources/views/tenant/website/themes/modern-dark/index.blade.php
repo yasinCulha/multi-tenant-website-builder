d3930b64e0342a3aa365a7281b1e9049e72da132
yasin culha <culhayasin732@gmail.com>
2026-07-09 23:04:09 +0300

    theme yapısı güncelleme7

--- resources/views/tenant/website/themes/modern-dark/index.blade.php
+++ resources/views/tenant/website/themes/modern-dark/index.blade.php
@@ -13,7 +13,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <!-- SweetAlert2 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <@vite(['resources/views/tenant/website/themes/modern-dark/assets/css/app.css',])
+@vite([
+    'resources/views/tenant/website/themes/modern-dark/assets/css/app.css',
+    'resources/views/tenant/website/themes/modern-dark/assets/js/app.js',
+])
 </head>
 <body>
     
@@ -33,7 +36,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- SweetAlert2 JS CDN -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-<@vite(['resources/views/tenant/website/themes/modern-dark/assets/js/app.js',])
 </body>
 </html>
