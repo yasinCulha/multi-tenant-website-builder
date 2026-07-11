d595b041f93a22bddef3e2cd1287a593c48a8f9f
yasin culha <culhayasin732@gmail.com>
2026-07-11 15:44:35 +0300

    oldu mu 16

---
+++ database/migrations/2026_07_11_124413_create_page_module_contents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('page_module_contents', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->foreignId('page_module_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('field_key');
+
+            $table->longText('field_value')
+                ->nullable();
+
+            $table->timestamps();
+
+            $table->unique([
+                'page_module_id',
+                'field_key'
+            ]);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('page_module_contents');
+    }
+};
